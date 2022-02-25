79cb8e88292c482100fd4f3bc04dfeeff3d3b57c
chouaibe22 <chouaib.chouache@univ-constantine2.dz>
2022-02-25 21:42:56 +0100

    menu exemple

---
+++ app.js
@@ -0,0 +1,25 @@
+// const btn = document.querySelector('.btn');
+// const nav = document.querySelector('#third-menu');
+
+
+// btn.addEventListener('click' , ()=>{
+
+
+// nav.classList.toggle('display-none');
+
+// })
+
+
+// nav.addEventListener('click' , ()=> nav.classList.add('display-none'))
+
+
+
+
+
+// const chouaibe = {
+//     name : 'chouaibe',
+//     brithday : 2000,
+//     // friends : new arrays('Ahmed' , 'mohamed' , 'ali'),
+
+//     calcage : age => 2022-age;
+// };
